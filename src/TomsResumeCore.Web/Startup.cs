b865be6bc5b848a9f2f695ef6a808baab0a21c0f
thutson <thutson79@gmail.com>
2019-06-11 12:14:28 -0400

    Add icon manifest mime type to startup.

--- src/TomsResumeCore.Web/Startup.cs
+++ src/TomsResumeCore.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -51,7 +52,14 @@ namespace TomsResumeCore.Web
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
+            FileExtensionContentTypeProvider provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".webmanifest"] = "application/manifest+json";
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                ContentTypeProvider = provider
+            });
             //app.UseCookiePolicy();
 
             app.UseMvc();
